1f52a87e9ed60122032f9496dc753032a7af7d9a
rishika2910 <rishikajagadesswaran@gmail.com>
2022-01-31 00:53:17 +0530

    dictornary added

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ PORT = os.environ["PY_PORT"]
 
 @app.route('/')
 def hello():
-    return 'Montreal'
+    result_dict={
+        city1="Montreal", 
+        city2="Qubec"
+    }
+    return result_dict
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = PORT)
